de95547ea6b9399d6ec6fe160e2513932963568e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-01-20 13:11:31 -0500

    Replaced " with '

--- src/cli/setup.js
+++ src/cli/setup.js
@@ -20,37 +20,37 @@ const web_1 = require("../../install/web");
 Object.defineProperty(exports, "webInstall", { enumerable: true, get: function () { return web_1.install; } });
 function setup(initConfig) {
     return __awaiter(this, void 0, void 0, function* () {
-        const { paths } = require("../constants");
-        const install = require("../install");
-        const build = require("../meta/build");
-        const prestart = require("../prestart");
-        const pkg = require("../../package.json");
-        winston_1.default.info("NodeBB Setup Triggered via Command Line");
+        const { paths } = require('../constants');
+        const install = require('../install');
+        const build = require('../meta/build');
+        const prestart = require('../prestart');
+        const pkg = require('../../package.json');
+        winston_1.default.info('NodeBB Setup Triggered via Command Line');
         console.log(`\nWelcome to NodeBB v${pkg.version}!`);
-        console.log("\nThis looks like a new installation, so you'll have to answer a few questions about your environment before we can proceed.");
-        console.log("Press enter to accept the default setting (shown in brackets).");
+        console.log('\nThis looks like a new installation, so you\'ll have to answer a few questions about your environment before we can proceed.');
+        console.log('Press enter to accept the default setting (shown in brackets).');
         install.values = initConfig;
         const data = yield install.setup();
         let configFile = paths.config;
         if (nconf_1.default.get("config")) {
-            configFile = path_1.default.resolve(paths.baseDir, nconf_1.default.get("config"));
+            configFile = path_1.default.resolve(paths.baseDir, nconf_1.default.get('config'));
         }
         prestart.loadConfig(configFile);
-        if (!nconf_1.default.get("skip-build")) {
+        if (!nconf_1.default.get('skip-build')) {
             yield build.buildAll();
         }
-        let separator = "     ";
+        let separator = '     ';
         if (process.stdout.columns > 10) {
             for (let x = 0, cols = process.stdout.columns - 10; x < cols; x += 1) {
-                separator += "=";
+                separator += '=';
             }
         }
         console.log(`\n${separator}\n`);
-        if (data.hasOwnProperty("password")) {
-            console.log("An administrative user was automatically created for you:");
+        if (data.hasOwnProperty('password')) {
+            console.log('An administrative user was automatically created for you:');
             console.log(`    Username: ${data.username}`);
             console.log(`    Password: ${data.password}`);
-            console.log("");
+            console.log('');
         }
         console.log('NodeBB Setup Completed. Run "./nodebb start" to manually start your NodeBB server.');
         // If I am a child process, notify the parent of the returned data before exiting (useful for notifying
